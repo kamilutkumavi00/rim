7abc9b51f603190c3198e07fe620be9641365e12
Kamil Utku Mavi <kamilutkumavi0@pop-os.localdomain>
2023-10-03 23:18:58 +0300

    dev

--- src/app.rs
+++ src/app.rs
@@ -5,15 +5,22 @@ pub struct App{
     data: Vec<String>,
     visual_data: Vec<String>,
     loc: (u16, u16, u16, u16),
-    motion_fn: fn(()),
+    motion_fn: fn((u16, u16, u16, u16)) -> (u16, u16, u16, u16),
 }
 
 impl App{
-    fn new(){
-
+    pub fn new(
+    start: (u16, u16),
+    size: (u16, u16),
+    loc: (u16, u16, u16, u16),
+    motion_fn: fn((u16, u16, u16, u16)) -> (u16, u16, u16, u16),
+    )-> Self{
+        let data = Vec::new();
+        let virtual_data = Vec::new();
+        Self { start: start, size: size, data: data, visual_data: virtual_data, loc: loc, motion_fn: motion_fn}
     }
 
-    fn update(){
-
+    pub fn update(mut self){
+        self.loc = (self.motion_fn)(self.loc);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use rim::{screen::Screen, window::Window, app::App};
+fn motions((x, y, x_s, y_s): (u16, u16, u16, u16)) -> (u16, u16, u16, u16){
+    (0,0,0,0)
+}
+fn visuals(s: (u16,u16), size:(u16,u16), data:Vec<String>){
+    let mut data_virtual: Vec<String> = Vec::new();
+    for i in s.1..s.1+size.1{
+        if i < data.len() as u16{
+            data_virtual.push(data[i as usize].clone()[s.0 as usize..s.0 as usize+size.0 as usize].to_string());
+        }
+    }
+    dbg!(data_virtual);
+}
 fn main() {
+    let mut vector: Vec<String> = Vec::new();
+    vector.push(String::from("Kamil Utku"));
+    vector.push(String::from("Kamil Utku"));
+    vector.push(String::from("Kamil Utku"));
+    vector.push(String::from("Kamil Utku"));
+    visuals((4,0), (5,5), vector);
     let mut screen = Screen::new();
 
     let text_editor = Window::new("Text Editor".to_string(), Vec::new(), (50,0), (50,8));
+    let a = App::new((0,0), (1,1), (0,0,0,0), motions);
+    a.update();
     screen = screen.add(text_editor);
+
 
     let file_explorer = Window::new("File Explorer".to_string(), Vec::new(), (0,0), (10,10));
     screen = screen.add(file_explorer);
