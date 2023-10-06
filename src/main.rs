258856f341a3482e08b20ae56a057b8e2ca8061d
Kamil Utku Mavi <kamilutkumavi0@pop-os.localdomain>
2023-10-06 09:21:22 +0300

    new

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,36 @@ fn main() {
     vector.push(String::from("Kamil Utku"));
     vector.push(String::from("Kamil Utku"));
     vector.push(String::from("Kamil Utku"));
-    let dv = visuals((0,0), (5,5), vector);
+    let dv = visuals((0,0), (10,5), vector);
+    let mut vector: Vec<String> = Vec::new();
+    vector.push(String::from("1"));
+    vector.push(String::from("2"));
+    vector.push(String::from("3"));
+    vector.push(String::from("4"));
+    let dv2 = visuals((0,0), (1,5), vector);
 
     let mut screen = Screen::new();
 
     let mut text_editor = Window::new("Text Editor".to_string(), Vec::new(), (50,0), (50,8));
-    let a = App::new((0,0), (10,10),dv ,(0,0,0,0), motions);
+    let a = App::new((4,1), (10,5),dv ,(0,0,0,0), motions);
+    let b = App::new((1,1), (1,5),dv2 ,(0,0,0,0), motions);
     text_editor = text_editor.add(a);
+    text_editor = text_editor.add(b);
     screen = screen.add(text_editor);
 
 
-    // let file_explorer = Window::new("File Explorer".to_string(), Vec::new(), (0,0), (10,10));
-    // screen = screen.add(file_explorer);
+    let mut vector: Vec<String> = Vec::new();
+    vector.push(String::from("1"));
+    vector.push(String::from("2"));
+    vector.push(String::from("3"));
+    vector.push(String::from("4"));
+    vector.push(String::from("5"));
+    let dv2 = visuals((0,0), (1,5), vector);
+    let mut file_explorer = Window::new("File Explorer".to_string(), Vec::new(), (0,0), (10,10));
+    let b = App::new((1,1), (1,5),dv2 ,(0,0,0,0), motions);
+    file_explorer = file_explorer.add(b);
+    screen = screen.add(file_explorer);
 
     screen = screen.update();
-    // screen.render();
+    screen.render();
 }
--- src/window.rs
+++ src/window.rs
@@ -10,7 +10,7 @@ pub struct Window{
     app_vec: Vec<App>,
 }
 
-fn window_framer(size: (u16, u16), visual_line: Vec<String>) -> Vec<String>{ // there is going to have a hash map for boxes
+fn window_framer(size: (u16, u16), visual_line: Vec<String>, dict: HashMap<(u16,u16), char>) -> Vec<String>{ // there is going to have a hash map for boxes
     let mut temp: Vec<String> = Vec::new();
     for j in 0..size.1{
         temp.push(String::new());
@@ -28,11 +28,7 @@ fn window_framer(size: (u16, u16), visual_line: Vec<String>) -> Vec<String>{ // 
             } else if i == 0 || i == size.0 - 1 {
                 temp[j as usize].push('┃');
             } else {
-                let a = if j <= visual_line.len() as u16{
-                    visual_line[j as usize - 1].chars().nth(i as usize - 1).unwrap_or(' ')
-                } else { 
-                    ' ' 
-                };
+                let a = *dict.get(&(i,j)).unwrap_or(&' ');
                 temp[j as usize].push(a);
             }
         }
@@ -50,30 +46,41 @@ impl Window{
         Self { name: self.name, lines: self.lines, start: self.start, size: self.size, visual_line: self.visual_line, app_vec: self.app_vec}
     }
 
-    pub fn update(self) -> Self { //motionlar kontrol edilecek ve visual appten çekilecek
-        make_dict(&self.app_vec, self.size);
-        let temp: Vec<String> = window_framer(self.size, self.visual_line);
+    pub fn update(mut self) -> Self { //motionlar kontrol edilecek ve visual appten çekilecek
+        let a = make_dict(&self.app_vec, self.size);
+        let temp: Vec<String> = window_framer(self.size, self.visual_line, a);
         Self { name: self.name, lines: self.lines, start: self.start, size: self.size, visual_line: temp, app_vec: self.app_vec}
     }
 
     //pub fn motions() -> Self {}
 }
 
-fn make_dict(app: &Vec<App>, size: (u16, u16)){
+fn make_dict(app: &Vec<App>, size: (u16, u16)) -> HashMap<(u16, u16), char>{
     let mut temp: HashMap<(u16,u16), char> = HashMap::new();
     for i in app{
         for j in 0..size.1{
             for k in 0..size.0{
-                if (j >= i.start.1 && j < i.start.1 + i.size.1) && (k >= i.start.0 && k < i.start.0 + i.size.0) && j < i.visual_data.len() as u16{
-                    let v = i.visual_data[j as usize - i.start.1 as usize].chars().nth(k as usize - i.start.0 as usize).unwrap_or(' ');
-                    temp.insert((k,j), v);
+                if (j >= i.start.1 && j < i.start.1 + i.size.1) && (k >= i.start.0 && k < i.start.0 + i.size.0) && j < i.visual_data.len() as u16 - i.start.1{
+                    if k - i.start.0 < i.visual_data[j as usize].len() as u16{
+                        let v = i.visual_data[j as usize - i.start.1 as usize].chars().nth(k as usize - i.start.0 as usize).unwrap();
+                        temp.insert((k,j), v);
+
+                    } else {
+                        if temp.get(&(k,j)) == None {
+                            temp.insert((k,j), ' ');
+                        } else {
+    
+                        }
+                    }
                 } else {
-                    if temp.get(&(k,j)) == None{
+                    if temp.get(&(k,j)) == None {
                         temp.insert((k,j), ' ');
+                    } else {
+
                     }
                 }
             }
         }
     }
-    dbg!(&temp.get(&(0,0)));
+    temp
 }
