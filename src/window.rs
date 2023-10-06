380983dc4666ffa7f98e87ea630751a6b8b77bae
Kamil Utku Mavi <kamilutkumavi0@pop-os.localdomain>
2023-10-06 09:33:03 +0300

    message

--- src/window.rs
+++ src/window.rs
@@ -10,7 +10,7 @@ pub struct Window{
     app_vec: Vec<App>,
 }
 
-fn window_framer(size: (u16, u16), visual_line: Vec<String>, dict: HashMap<(u16,u16), char>) -> Vec<String>{ // there is going to have a hash map for boxes
+fn window_framer(size: (u16, u16), _visual_line: Vec<String>, dict: HashMap<(u16,u16), char>) -> Vec<String>{ // there is going to have a hash map for boxes
     let mut temp: Vec<String> = Vec::new();
     for j in 0..size.1{
         temp.push(String::new());
@@ -46,7 +46,7 @@ impl Window{
         Self { name: self.name, lines: self.lines, start: self.start, size: self.size, visual_line: self.visual_line, app_vec: self.app_vec}
     }
 
-    pub fn update(mut self) -> Self { //motionlar kontrol edilecek ve visual appten çekilecek
+    pub fn update(self) -> Self { //motionlar kontrol edilecek ve visual appten çekilecek
         let a = make_dict(&self.app_vec, self.size);
         let temp: Vec<String> = window_framer(self.size, self.visual_line, a);
         Self { name: self.name, lines: self.lines, start: self.start, size: self.size, visual_line: temp, app_vec: self.app_vec}
@@ -58,10 +58,11 @@ impl Window{
 fn make_dict(app: &Vec<App>, size: (u16, u16)) -> HashMap<(u16, u16), char>{
     let mut temp: HashMap<(u16,u16), char> = HashMap::new();
     for i in app{
+        dbg!(i.visual_data.len());
         for j in 0..size.1{
             for k in 0..size.0{
-                if (j >= i.start.1 && j < i.start.1 + i.size.1) && (k >= i.start.0 && k < i.start.0 + i.size.0) && j < i.visual_data.len() as u16 - i.start.1{
-                    if k - i.start.0 < i.visual_data[j as usize].len() as u16{
+                if (j >= i.start.1 && j < i.start.1 + i.size.1) && (k >= i.start.0 && k < i.start.0 + i.size.0) && j - i.start.1 < i.visual_data.len() as u16 {
+                    if k - i.start.0 < i.visual_data[j as usize - i.start.1 as usize].len() as u16{
                         let v = i.visual_data[j as usize - i.start.1 as usize].chars().nth(k as usize - i.start.0 as usize).unwrap();
                         temp.insert((k,j), v);
 
