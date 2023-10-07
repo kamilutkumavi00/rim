bb772aadf131de2de2ea7cb462d95f57a18bb825
Kamil Utku Mavi <kamilutkumavi0@pop-os.localdomain>
2023-10-07 21:06:05 +0300

    m

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use rim::{screen::Screen, window::Window, app::App};
+
 fn motions((_x, _y, _x_s, _y_s): (u16, u16, u16, u16)) -> (u16, u16, u16, u16){
     (0,0,0,0)
 }
-fn visuals(s: (u16,u16), size:(u16,u16), data:Vec<String>) -> Vec<String>{
+fn visuals(s: (u16,u16), size:(u16,u16), mut data:Vec<String>) -> Vec<String>{
     let mut data_virtual: Vec<String> = Vec::new();
     for i in s.1..s.1+size.1{
         if i < data.len() as u16{
-            data_virtual.push(data[i as usize].clone()[s.0 as usize..s.0 as usize+size.0 as usize].to_string());
+            if data[i as usize].len() >= s.0 as usize+size.0 as usize{
+                data_virtual.push(data[i as usize].clone()[s.0 as usize..s.0 as usize+size.0 as usize].to_string());
+            } else {
+                data_virtual.push(data[i as usize].clone()[s.0 as usize..data[i as usize].len()].to_string());
+            }
         }
     }
     data_virtual
 }
 fn main() {
     let mut vector: Vec<String> = Vec::new();
-    vector.push(String::from("Kamil Utku"));
+    vector.push(String::from("Kamil U"));
     vector.push(String::from("Kamil Utku"));
     vector.push(String::from("Kamil Utku"));
     vector.push(String::from("Kamil Utku"));
@@ -27,7 +32,7 @@ fn main() {
 
     let mut screen = Screen::new();
 
-    let mut text_editor = Window::new("Text Editor".to_string(), Vec::new(), (50,0), (50,8));
+    let mut text_editor = Window::new("Text Editor".to_string(), Vec::new(), (80,0), (50,8));
     let a = App::new((4,1), (10,5),dv ,(0,0,0,0), motions);
     let b = App::new((1,1), (1,5),dv2 ,(0,0,0,0), motions);
     text_editor = text_editor.add(a);
